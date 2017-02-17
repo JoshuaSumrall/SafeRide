8870915e46a53c907ba05db07e44a84cc80cb893
Joshua Sumrall <jsumrall@gmail.com>
2017-02-17 03:49:58 +0000

    Corrected typo in app.js

--- src/app.js
+++ src/app.js
@@ -6,18 +6,18 @@ export class App {
     this.todos = [];
     this.todoDescription = '';
   }
-}
 
-addTodo(){
-    if (this.todoDescription) {
-        this.todos.push(new Todo(this.todoDescription));
-        this.todoDescription = '';
+    addTodo() {
+        if (this.todoDescription) {
+            this.todos.push(new Todo(this.todoDescription));
+            this.todoDescription = '';
+        }
     }
-}
 
-removeTodo(todo) {
-    let index = this.todos.indexOf(todo);
-    if (index != -1) {
-        this.todos.splice(index, 1);
+    removeTodo(todo) {
+        let index = this.todos.indexOf(todo);
+        if (index !== -1) {
+            this.todos.splice(index, 1);
+        }
     }
 }

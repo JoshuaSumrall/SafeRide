5682498a50de77f5c5e9e10d148262e3af3003b6
Joshua Sumrall <jsumrall@gmail.com>
2017-03-09 17:13:39 -0600

    Format passneger.js

--- src/passenger.js
+++ src/passenger.js
@@ -4,14 +4,17 @@ export class Passenger {
         this.lastName = LastName;
         this.nickName = NickName;
     }
+    
     get FirstName() { return this.firstName; }
     get LastName() { return this.lastName; }
     get NickName() { return this.nickName; }
     get IsPickedUp() { return this.isPickedUp; }
     get IsDroppedOff() { return this.isDroppedOff; }
+    
     set FirstName(FirstName) { this.firstName = FirstName; }
     set LastName(LastName) { this.lastName = LastName; }
     set NickName(NickName) { this.nickName = nickName; }
     set IsPickedUp(IsPickedUp) { this.isPickedUp = IsPickedUp; }
     set IsDroppedOff(IsDroppedOff) { this.isDroppedOff = IsDroppedOff; }
 }
+
